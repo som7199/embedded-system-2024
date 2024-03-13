eec1ddc28940223893f64ee5f0bd934c67f33e21
som7199 <95500015+som7199@users.noreply.github.com>
2024-03-13 18:08:45 +0900

    Create homework02.c

---
+++ day01/homework02.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int minus(int a, int b)
+{
+    if (a > b) { return a - b; }
+    else { return b - a; }
+}
+
+int multiple(int a, int b)
+{
+    return a * b;
+}
+
+float divide(int a, int b)
+{
+    return a / b;
+}
+
+
+void main()
+{
+    while (1) {
+        int num1, num2;
+        char op;
+        printf("\n두 개의 정수를 입력하시오>");
+        scanf("%d %d", &num1, &num2);
+        printf("연산자를 입력하시오 > (종료는 X)");
+        scanf(" %c", &op);
+
+        if (op == '+') {
+            printf("%d", add(num1, num2));
+        }
+        else if (op == '-') {
+            printf("%d", minus(num1, num2));
+        }
+        else if (op == '*') {
+            printf("%d", multiple(num1, num2));
+        }
+        else if (op == '/') {
+            printf("%.1f", divide(num1, num2));
+        }
+        else if (op == 'X') {
+            printf("프로그램 종료\n");
+            break;
+        }
+    }
+}
