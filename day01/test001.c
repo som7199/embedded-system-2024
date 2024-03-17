e7c04adf9f59354025afc02227ad275e567e9ea0
som7199 <95500015+som7199@users.noreply.github.com>
2024-03-17 16:11:36 +0900

    Update test001.c

--- day01/test001.c
+++ day01/test001.c
@@ -1,29 +1,29 @@
- #include <stdio.h>
- 2 #define MAX 20
- 3
- 4 void main()
- 5 {
- 6   int arr[MAX];
- 7   int num, inNum;
- 8
- 9   printf("Input Data >> ");
-10   scanf("%d", &num);
-11   printf("배열 입력\n");
-12   for(int i = 0; i < num; i++) {
-13     scanf("%d", &inNum);
-14     arr[i] = inNum;
-15   }
-16   //배열 출력
-17   printf("순차 출력 > ");
-18   for(int i = 0; i < num; i++) {
-19     printf("%d ", arr[i]);
-20   }
-21   printf("\n");
-22
-23   // 역순으로 출력
-24   printf("역순 출력 > ");
-25   for(int i = num - 1; i > -1; i--) {
-26     printf("%d ", arr[i]);
-27   }
-28   printf("\n");
-29 }
+#include <stdio.h>
+#define MAX 20
+
+void main()
+{
+  int arr[MAX];
+  int num, inNum;
+
+  printf("Input Data >> ");
+  scanf("%d", &num);
+  printf("배열 입력\n");
+  for(int i = 0; i < num; i++) {
+    scanf("%d", &inNum);
+    arr[i] = inNum;
+  }
+  //배열 출력
+  printf("순차 출력 > ");
+  for(int i = 0; i < num; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+
+  // 역순으로 출력
+  printf("역순 출력 > ");
+  for(int i = num - 1; i > -1; i--) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
